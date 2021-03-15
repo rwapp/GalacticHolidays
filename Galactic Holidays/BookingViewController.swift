5c2bb346ce83b480cc27e49de5d962d845f2484d
rwapp <r.whitaker@mac.com>
2021-03-15 21:08:31 +0000

    booking screen

--- Galactic Holidays/BookingViewController.swift
+++ Galactic Holidays/BookingViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 class BookingViewController: UIViewController {
 
     @IBOutlet weak private var submit: UIButton!
+    @IBOutlet weak private var emailField: TextField!
+    @IBOutlet weak private var confirmEmailField: TextField!
+    @IBOutlet weak private var nameField: TextField!
+    @IBOutlet weak private var cardNumberField: TextField!
+    @IBOutlet weak private var cvvField: TextField!
+    @IBOutlet weak private var sortCodeField: TextField!
+    @IBOutlet weak private var dobField: TextField!
+    @IBOutlet weak private var loadingView: UIView!
+    @IBOutlet weak private var scrollView: UIScrollView!
 
-    private var alert: CustomModalAlert?
+    private var timer: Timer?
+    weak var activeField: UITextField?
 
-    private var formValid: Bool = false {
+    private var alert: CustomModalAlert? {
         didSet {
-            submit.isEnabled = formValid
+            oldValue?.removeFromSuperview()
+        }
+    }
+
+    private var formComplete: Bool = false {
+        didSet {
+            submit.isEnabled = formComplete
         }
     }
 
@@ -27,12 +43,20 @@ class BookingViewController: UIViewController {
             $0.delegate = self
         }
 
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+
         idleTimer()
     }
 
     private func idleTimer() {
-
-        _ = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: false) { [weak self] _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: false) { [weak self] _ in
             guard let self = self else { return }
 
             self.showTimeout()
@@ -40,12 +64,29 @@ class BookingViewController: UIViewController {
         }
     }
 
+    private func bookingTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { [weak self] _ in
+            guard let self = self else { return }
+
+            self.loadingView.isHidden = false
+            self.performSegue(withIdentifier: "bookingComplete", sender: nil)
+        }
+    }
+
     private func showTimeout() {
-        let alert = CustomModalAlert(frame: view.frame)
-        alert.delegate = self
-        alert.center = view.center
-        self.alert = alert
-        view.addSubview(alert)
+        alert = CustomModalAlert(frame: view.frame)
+        alert!.delegate = self
+        alert!.message = "Timed out"
+        alert!.center = view.center
+        view.addSubview(alert!)
+    }
+
+    private func showFormError() {
+        alert = CustomModalAlert(frame: view.frame)
+        alert!.delegate = self
+        alert!.message = "Check form"
+        alert!.center = view.center
+        view.addSubview(alert!)
     }
 
     private func clearTextFields() {
@@ -54,25 +95,109 @@ class BookingViewController: UIViewController {
         }
     }
 
-    private func validateForm() {
-        var formValid = true
+    private func checkFormComplete() {
+        var formComplete = true
 
         eachTextField(view) {
             if $0.text?.count ?? 0 == 0 {
-                formValid = false
+                formComplete = false
             }
         }
 
-        self.formValid = formValid
+        self.formComplete = formComplete
+    }
+
+    private func checkFormValid() -> Bool {
+                guard let email = emailField.text,
+                      let confirmEmail = confirmEmailField.text,
+                      let name = nameField.text,
+                      let cardNumber = cardNumberField.text,
+                      let cvv = cvvField.text,
+                      let sortCode = sortCodeField.text,
+                      let dob = dobField.text else { return false }
+
+                if email != confirmEmail { return false }
+                if !email.contains("@") { return false }
+                if !confirmEmail.contains("@") { return false }
+                if !email.contains(".com") { return false }
+                if !confirmEmail.contains(".com") { return false }
+
+                if name.count < 3 { return false }
+
+                if cardNumber.count != 19 { return false }
+                if !charAt(location: 4, is: " ", in: cardNumber) { return false }
+                if !charAt(location: 9, is: " ", in: cardNumber) { return false }
+                if !charAt(location: 14, is: " ", in: cardNumber) { return false }
+
+                if cvv.count != 3 { return false }
+                if cvv.rangeOfCharacter(from: .lowercaseLetters) != .none { return false }
+
+                if sortCode.count != 8 { return false }
+                if !charAt(location: 3, is: "-", in: sortCode) { return false }
+                if !charAt(location: 6, is: "-", in: sortCode) { return false }
+
+                if dob.count != 10 { return false }
+                if !charAt(location: 3, is: "/", in: dob) { return false }
+                if !charAt(location: 6, is: "/", in: dob) { return false }
+
+        return true
+    }
+
+    private func charAt(location: Int, is character: Character, in string: String) -> Bool {
+        let stringChar = string[string.index(string.startIndex, offsetBy: location)]
+        return stringChar == character
+    }
+
+    @IBAction
+    private func bookPressed() {
+        if checkFormValid() {
+            timer?.invalidate()
+            processBooking()
+        } else {
+            showFormError()
+        }
+    }
+
+    private func processBooking() {
+        loadingView.isHidden = false
+        bookingTimer()
     }
 
     private func eachTextField(_ subview: UIView, _ action: (TextField) -> Void) {
         for view in subview.subviews {
-            eachTextField(view, action)
             if let textField = view as? TextField {
                 action(textField)
+            } else {
+                eachTextField(view, action)
             }
         }
+    }
+
+    @objc
+    func keyboardDidShow(notification: Notification) {
+        guard let keyboardRect: CGRect = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect else { return }
+        let keyboardHeight = keyboardRect.size.height
+
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+
+        if let activeField = activeField {
+            var visibleArea = view.frame
+            visibleArea.size.height -= keyboardHeight
+
+            if !visibleArea.contains(activeField.frame.origin) {
+                let scrollPoint = CGPoint(x: 0, y: activeField.frame.origin.y)
+                scrollView.setContentOffset(scrollPoint, animated: true)
+            }
+        }
+    }
+
+    @objc
+    func keyboardWillHide() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        })
     }
 
     private func timedOut() {
@@ -82,13 +207,19 @@ class BookingViewController: UIViewController {
 
 extension BookingViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
-        validateForm()
+        checkFormComplete()
+        activeField = nil
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
     }
 }
 
 extension BookingViewController: CustomModalAlertDelegate {
     func dismiss() {
         alert?.removeFromSuperview()
+        timer?.invalidate()
         idleTimer()
     }
 }
--- Galactic Holidays/TextField.swift
+++ Galactic Holidays/TextField.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class TextField: UITextField {
 
+    private var line: UIView?
+
     override func draw(_ rect: CGRect) {
+        super.draw(rect)
         borderStyle = .none
 
-        let lineFrame = CGRect(x: 0, y: rect.size.height - 10, width: rect.size.width, height: 1)
+        sizeToFit()
+
+        line?.removeFromSuperview()
+        let lineFrame = CGRect(x: 0, y: frame.size.height - 1, width: rect.size.width, height: 1)
         let line = UIView(frame: lineFrame)
         line.backgroundColor = .blue
 
         addSubview(line)
+        self.line = line
     }
 
 }
