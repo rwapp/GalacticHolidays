70d4d2f884865ce6c6cfab9160208a4446d0cfc7
rwapp <r.whitaker@mac.com>
2021-05-23 19:06:11 +0100

    success screen

--- Galactic Holidays/ViewControllers/SuccessViewController.swift
+++ Galactic Holidays/ViewControllers/SuccessViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class SuccessViewController: UIViewController {
 
     @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var backButton: UIButton!
+    @IBOutlet weak private var referenceLabel: UILabel!
+    @IBOutlet weak private var titleContainer: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,21 @@ class SuccessViewController: UIViewController {
         let image = UIImage(named: "space")?.withRenderingMode(.alwaysTemplate)
         imageView.image = image
         imageView.tintColor = .white
+
+        let reference = "123ABC456"
+        let referenceString = NSMutableAttributedString(string: "Booking ref: ")
+        let attributedReference = NSAttributedString(string: reference, attributes: [.accessibilitySpeechSpellOut: true])
+        referenceString.append(attributedReference)
+        referenceLabel.attributedText = referenceString
+
+        let backButtonTitle = NSAttributedString(string: "Go back home", attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
+        backButton.titleLabel?.attributedText = backButtonTitle
+        backButton.accessibilityTraits.insert(.link)
+        backButton.accessibilityTraits.remove(.button)
+
+        titleContainer.isAccessibilityElement = true
+        titleContainer.accessibilityTraits.insert(.header)
+        titleContainer.accessibilityLabel = "You're going!"
     }
 
     override func viewWillAppear(_ animated: Bool) {
